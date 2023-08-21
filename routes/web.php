e05752b558e16f246bd9e2c95f430295cd03dd65
Matteo Sarrecchia <morrea@hotmail.it>
2023-08-21 14:57:31 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\LoggedController;
+
+
+Route :: get('/', [MainController :: class, 'index']) -> name('welcome');
+
+Route :: get('/show/{id}', [LoggedController :: class, 'show']) -> name('show');
+
+Route :: get('/create', [LoggedController :: class, 'create'])
+    -> middleware(['auth'])
+    -> name('create');
+
+Route :: post('/store', [LoggedController :: class, 'store'])
+    -> middleware(['auth'])
+    -> name('store');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
